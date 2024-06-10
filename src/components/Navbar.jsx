c699c4ba485e43059f93eee96621e6aacaefdfd6
Max de Vocht <maxdevocht@hotmail.com>
2024-06-10 16:57:31 +0200

    Created Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Compass } from "lucide-react"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <Compass />
+        <h1>Jetlinker</h1> 
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
